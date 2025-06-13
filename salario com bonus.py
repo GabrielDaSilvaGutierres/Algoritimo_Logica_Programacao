a6804598c68c523ad88aca51e957398ab03d24f1
Gabriel Da Silva Gutierres/Dice <gabriel.gutierres@fatec.sp.gov.br>
2025-06-13 10:40:30 -0300

    exercicios beecrowd

---
+++ salario com bonus.py
@@ -0,0 +1,7 @@
+n = str(input())
+sf= float(input())
+tv = float(input())
+
+salary = ht * sh
+
+print(f'SALARY = U$ {salary:.2f}')
---
+++ salário.py
@@ -0,0 +1,8 @@
+n = int(input())
+ht = int(input())
+sh = float(input())
+
+salary = ht * sh
+
+print(f'NUMBER = {n}')
+print(f'SALARY = U$ {salary:.2f}')
